b6b91a3f493a4eb3cf0b57d684348e7c8411d6da
zpica20 <zpica2004@gmail.com>
2025-12-05 11:04:56 -0500

    Add POST route to generate a story from user input
    Refactor index.js to handle POST requests and generate a story based on user input.

--- index.js
+++ index.js
@@ -1,6 +1,57 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
-app.get("/", (req, res) => {res.send("Hello World!");});
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`);});
 
+// Needed to read POST form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/", (req, res) => {
+
+    const {
+        noun1,
+        verb1,
+        adjective1,
+        animal1,
+        verb2,
+        place1,
+        adjective2
+    } = req.body;
+
+    const express = require("express");
+    const app = express();
+    const PORT = process.env.PORT || 8080;
+    
+    // Needed to read POST form data
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+    
+    app.post("/", (req, res) => {
+
+    const {
+        noun1,
+        verb1,
+        adjective1,
+        animal1,
+        verb2,
+        place1,
+        adjective2
+    } = req.body;
+
+    const story = `One day a ${adjective1} ${noun1} decided to ${verb1}. 
+    Suddenly, a ${adjective2} ${animal1} appeared and started to ${verb2} at the ${place1}.`;
+
+    res.send(story);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+;
+
+    res.send(story);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
