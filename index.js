57635e04635327275bda2588b4c65f1f6d52e986
zpica20 <zpica2004@gmail.com>
2025-12-05 12:11:20 -0500

    Refactor index.js to simplify story generation

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
 app.post("/", (req, res) => {
 
     const {
@@ -18,28 +21,7 @@ app.post("/", (req, res) => {
         adjective2
     } = req.body;
 
-    const express = require("express");
-    const app = express();
-    const PORT = process.env.PORT || 8080;
-    
-    // Needed to read POST form data
-    app.use(express.urlencoded({ extended: true }));
-    app.use(express.json());
-    
-    app.post("/", (req, res) => {
-
-    const {
-        noun1,
-        verb1,
-        adjective1,
-        animal1,
-        verb2,
-        place1,
-        adjective2
-    } = req.body;
-
-    const story = `One day a ${adjective1} ${noun1} decided to ${verb1}. 
-    Suddenly, a ${adjective2} ${animal1} appeared and started to ${verb2} at the ${place1}.`;
+    const story = `One day a ${adjective1} ${noun1} decided to ${verb1}. Suddenly, a ${adjective2} ${animal1} appeared and started to ${verb2} at the ${place1}.`;
 
     res.send(story);
 });
@@ -47,11 +29,4 @@ app.post("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-;
 
-    res.send(story);
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
